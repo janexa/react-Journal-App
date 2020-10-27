278465ec6eec22a2322911ffe576589436ed8984
Janexa <jnxacc7@gmail.com>
2020-10-26 18:32:57 -0600

    edit f

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -4,13 +4,13 @@ import 'firebase/auth';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDFXW2W2SNV0lmj8HJogeu267hdD2HR5WE",
-    authDomain: "react-app-cursos.firebaseapp.com",
-    databaseURL: "https://react-app-cursos.firebaseio.com",
-    projectId: "react-app-cursos",
-    storageBucket: "react-app-cursos.appspot.com",
-    messagingSenderId: "959679322615",
-    appId: "1:959679322615:web:89d8e604eee1b5fd68f6f2"
+    apiKey: "AIzaSyCoRORn6SWZ_olq50HQuKsrOpTe9Dhjgew",
+    authDomain: "react-journal-dac63.firebaseapp.com",
+    databaseURL: "https://react-journal-dac63.firebaseio.com",
+    projectId: "react-journal-dac63",
+    storageBucket: "react-journal-dac63.appspot.com",
+    messagingSenderId: "1085891388105",
+    appId: "1:1085891388105:web:f371cf82b442f861aaa318"
 };
   
 firebase.initializeApp(firebaseConfig);
